780801791b5b709d44e36cd3a93c515590ad3772
m19e <nexus.me7290@gmail.com>
2021-08-04 02:29:46 +0900

    Prettier

--- commands/index.tsx
+++ commands/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Text} from 'ink';
+import React from "react";
+import PropTypes from "prop-types";
+import { Text } from "ink";
 
 /// Hello world command
-const Hello = ({name}) => <Text>Hello, {name}</Text>;
+const Hello = ({ name }) => <Text>Hello, {name}</Text>;
 
 Hello.propTypes = {
 	/// Name of the person to greet
-	name: PropTypes.string.isRequired
+	name: PropTypes.string.isRequired,
 };
 
 export default Hello;
