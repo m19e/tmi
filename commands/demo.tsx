d068e7f5d331bf46a4d094e6f8e27a28a223c66e
m19e <nexus.me7290@gmail.com>
2021-08-24 05:33:50 +0900

    Test each emojis

--- commands/demo.tsx
+++ commands/demo.tsx
@@ -3,21 +3,29 @@ import { Box, Text } from "ink";
 import { splitGraphemes } from "split-graphemes";
 
 const Demo = () => (
-	<Box flexDirection="column">
-		{splitGraphemes("𝙧𝙖𝐑𝐫𝙄𝙉𝙐🧙‍♀🌤🤏🏻🙆🏻‍♀️🙇🏻‍♀️🤲🏻🥲").map((line, i) => (
-			<Box key={i} borderStyle="round" borderColor="white">
-				{/* {([...line].length !== 1 || /[\u{1d400}-\u{1d7ff}]/u.test(line)) && (
+	<Box flexDirection="column" paddingY={2}>
+		{splitGraphemes("𝙧𝙖𝐑𝐫𝙄𝙉𝙐🧙‍♀🌤🤏🏻🙆🏻‍♀️🙇🏻‍♀️🤲🏻🥰🥱🥲🥳🥴🥵🥶🥷🥸🥺🧊🤍𓈒𓂂✌").map(
+			(line, i) => (
+				<Box key={i} borderStyle="round" borderColor="white">
+					{/* {([...line].length !== 1 || /[\u{1d400}-\u{1d7ff}]/u.test(line)) && (
 					<Text>
 						{[...line].map((c) => c.codePointAt(0).toString(16)).join() + "　"}
 					</Text>
 				)} */}
-				<Text>
-					{[...line]
-						.filter((c) => !/[\u{1f3fb}-\u{1f3ff}\ufe0f]/u.test(c))
-						.join("") + "　"}
-				</Text>
-			</Box>
-		))}
+					<Text>
+						{[...line].map((c) => c.codePointAt(0).toString(16)).join() + "　"}
+					</Text>
+					<Text>
+						{[...line]
+							.filter(
+								(c) =>
+									!/[\u{1d400}-\u{1d7ff}\u{1f3fb}-\u{1f3ff}\u{fe0f}]/u.test(c)
+							)
+							.join("")}
+					</Text>
+				</Box>
+			)
+		)}
 	</Box>
 );
 
