fe36811950030b91ec95937a77614b0d15aa8904
m19e <nexus.me7290@gmail.com>
2021-08-24 06:50:15 +0900

    Remove commented

--- commands/demo.tsx
+++ commands/demo.tsx
@@ -6,11 +6,6 @@ const Demo = () => (
 	<Box flexDirection="column" paddingY={2}>
 		{splitGraphemes("𝙧𝙖𝐑𝐫𝙄𝙉𝙐🧙‍♀🌤🤏🏻🙆🏻‍♀️🙇🏻‍♀️🤲🏻🧊🤍𓈒𓂂✌🩰🪘🪚🫖🐿").map((line, i) => (
 			<Box key={i} borderStyle="round" borderColor="white">
-				{/* {([...line].length !== 1 || /[\u{1d400}-\u{1d7ff}]/u.test(line)) && (
-					<Text>
-						{[...line].map((c) => c.codePointAt(0).toString(16)).join() + "　"}
-					</Text>
-				)} */}
 				<Text>
 					{[...line].map((c) => c.codePointAt(0).toString(16)).join() + "　"}
 				</Text>
