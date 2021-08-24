79a1fd691bcdbba4c3dcd6c81401fa3897953e89
m19e <nexus.me7290@gmail.com>
2021-08-25 07:55:26 +0900

    Remove duplicate emoji in msap regex

--- commands/demo.tsx
+++ commands/demo.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from "ink";
 import { splitGraphemes } from "split-graphemes";
 
 const general_reg =
-	/[\u{1d400}-\u{1d7ff}\u{1f3fb}-\u{1f3ff}\u{1f43f}\u{fe0f}\u{1f3d6}\u{1fad6}\u{1fa9a}\u{1fa98}\u{361}\u{35c}\u{32e}]/u;
+	/[\u{1d400}-\u{1d7ff}\u{1f3fb}-\u{1f3ff}\u{fe0f}\u{1fad6}\u{1fa9a}\u{1fa98}\u{361}\u{35c}\u{32e}]/u;
 
 // Block = “Miscellaneous Symbols and Pictographs”
 const msap_reg = new RegExp(
