d51f1dc6f855da340f53b0f299b3183a35611e15
m19e <nexus.me7290@gmail.com>
2021-08-27 03:13:58 +0900

    Shift to root regex

--- commands/demo.tsx
+++ commands/demo.tsx
@@ -31,6 +31,8 @@ const reg = new RegExp(
 		"\u{1f321}-\u{1f32c}\u{1f336}\u{1f37d}\u{1f394}-\u{1f39f}\u{1f3cd}\u{1f3ce}\u{1f3d4}-\u{1f3df}\u{1f3f1}-\u{1f3f3}\u{1f3f5}-\u{1f3f7}\u{1f43f}\u{1f441}\u{1f4fe}\u{1f4fd}\u{1f53e}-\u{1f54a}\u{1f54f}\u{1f568}-\u{1f573}\u{1f576}-\u{1f579}\u{1f57b}-\u{1f58f}\u{1f591}-\u{1f594}\u{1f597}-\u{1f5a3}\u{1f5a5}-\u{1f5fa}" +
 		// "Ornamental Dingbats"
 		"\u{1f650}-\u{1f67f}" +
+		// "Transport and Map Symbols"
+		"\u{1f6c6}-\u{1f6cb}\u{1f6cd}-\u{1f6cf}\u{1f6d3}\u{1f6d4}\u{1f6d6}-\u{1f6ea}\u{1f6f0}-\u{1f6f3}\u{1f6fb}-\u{1f6fc}" +
 		// "Alchemical Symbols"
 		"\u{1f700}-\u{1f77f}" +
 		// "Geometric Shapes Extended"
@@ -43,19 +45,6 @@ const reg = new RegExp(
 		"\u{1fb00}-\u{1fbff}" +
 		// "Variation Selectors Supplement"
 		"\u{e0100}-\u{e01ef}" +
-		"]",
-	"u"
-);
-
-// Block = "Transport and Map Symbols"
-const tm_reg = new RegExp(
-	"[" +
-		"\u{1f6c6}-\u{1f6cb}" +
-		"\u{1f6cd}-\u{1f6cf}" +
-		"\u{1f6d3}\u{1f6d4}" +
-		"\u{1f6d6}-\u{1f6ea}" +
-		"\u{1f6f0}-\u{1f6f3}" +
-		"\u{1f6fb}-\u{1f6fc}" +
 		"]",
 	"u"
 );
