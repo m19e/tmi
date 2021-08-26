7013818a50961a566675e2011f49d77c55e76c90
m19e <nexus.me7290@gmail.com>
2021-08-26 20:52:39 +0900

    Remove needless range

--- commands/demo.tsx
+++ commands/demo.tsx
@@ -4,7 +4,7 @@ import { Box, Text } from "ink";
 import { splitGraphemes } from "split-graphemes";
 
 const general_reg =
-	/[\u{1}-\u{6}\u{8}-\u{c}\u{10}-\u{1f}\u{84}\u{85}\u{8c}\u{90}\u{98}\u{9b}\u{9d}-\u{9f}\u{300}-\u{36f}\u{10000}-\u{1254f}\u{13000}-\u{1467f}\u{16800}-\u{18d8f}\u{1b002}-\u{1bcaf}\u{1d000}-\u{1daaf}\u{1e000}-\u{1f003}\u{1f005}-\u{1f18d}\u{1f18f}\u{1f190}\u{1f19b}-\u{1f1e5}\u{1f260}-\u{1f265}\u{1f3fb}-\u{1f3ff}\u{1f700}-\u{1f77f}\u{1f780}-\u{1f7df}\u{1f800}-\u{1f8ff}\u{1fa00}-\u{1fa6f}\u{1fb00}-\u{1fbff}\u{e0100}-\u{e01ef}]/u;
+	/[\u{1}-\u{6}\u{8}-\u{c}\u{10}-\u{1f}\u{84}\u{85}\u{8c}\u{90}\u{98}\u{9b}\u{9d}-\u{9f}\u{300}-\u{36f}\u{10000}-\u{1254f}\u{13000}-\u{1467f}\u{16800}-\u{18d8f}\u{1b002}-\u{1bcaf}\u{1d000}-\u{1daaf}\u{1e000}-\u{1f003}\u{1f005}-\u{1f18d}\u{1f18f}\u{1f190}\u{1f19b}-\u{1f1e5}\u{1f260}-\u{1f265}\u{1f700}-\u{1f77f}\u{1f780}-\u{1f7df}\u{1f800}-\u{1f8ff}\u{1fa00}-\u{1fa6f}\u{1fb00}-\u{1fbff}\u{e0100}-\u{e01ef}]/u;
 
 // Block = "Basic Latin"
 const bl_reg = new RegExp(
