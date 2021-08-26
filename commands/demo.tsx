3e0c218bab0d2e0496bff76c556b37f56944bde4
m19e <nexus.me7290@gmail.com>
2021-08-27 03:12:34 +0900

    Shift to root regex

--- commands/demo.tsx
+++ commands/demo.tsx
@@ -27,6 +27,8 @@ const reg = new RegExp(
 		"\u{1e000}-\u{1f003}\u{1f005}-\u{1f18d}\u{1f18f}\u{1f190}\u{1f19b}-\u{1f1e5}" +
 		// "Enclosed Ideographic Supplement"
 		"\u{1f260}-\u{1f265}" +
+		// "Miscellaneous Symbols and Pictographs"
+		"\u{1f321}-\u{1f32c}\u{1f336}\u{1f37d}\u{1f394}-\u{1f39f}\u{1f3cd}\u{1f3ce}\u{1f3d4}-\u{1f3df}\u{1f3f1}-\u{1f3f3}\u{1f3f5}-\u{1f3f7}\u{1f43f}\u{1f441}\u{1f4fe}\u{1f4fd}\u{1f53e}-\u{1f54a}\u{1f54f}\u{1f568}-\u{1f573}\u{1f576}-\u{1f579}\u{1f57b}-\u{1f58f}\u{1f591}-\u{1f594}\u{1f597}-\u{1f5a3}\u{1f5a5}-\u{1f5fa}" +
 		// "Ornamental Dingbats"
 		"\u{1f650}-\u{1f67f}" +
 		// "Alchemical Symbols"
@@ -41,31 +43,6 @@ const reg = new RegExp(
 		"\u{1fb00}-\u{1fbff}" +
 		// "Variation Selectors Supplement"
 		"\u{e0100}-\u{e01ef}" +
-		"]",
-	"u"
-);
-
-// Block = “Miscellaneous Symbols and Pictographs”
-const m_reg = new RegExp(
-	"[" +
-		"\u{1f321}-\u{1f32c}" +
-		"\u{1f336}" +
-		"\u{1f37d}" +
-		"\u{1f394}-\u{1f39f}" +
-		"\u{1f3cd}\u{1f3ce}" +
-		"\u{1f3d4}-\u{1f3df}" +
-		"\u{1f3f1}-\u{1f3f3}" +
-		"\u{1f3f5}-\u{1f3f7}" +
-		"\u{1f43f}\u{1f441}" +
-		"\u{1f4fe}\u{1f4fd}" +
-		"\u{1f53e}-\u{1f54a}" +
-		"\u{1f54f}" +
-		"\u{1f568}-\u{1f573}" +
-		"\u{1f576}-\u{1f579}" +
-		"\u{1f57b}-\u{1f58f}" +
-		"\u{1f591}-\u{1f594}" +
-		"\u{1f597}-\u{1f5a3}" +
-		"\u{1f5a5}-\u{1f5fa}" +
 		"]",
 	"u"
 );
