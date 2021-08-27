76906fe1bd3d37cf01c68f2bdb1e4b63e1b336f0
m19e <nexus.me7290@gmail.com>
2021-08-27 12:33:21 +0900

    Call use-app hook

--- commands/index.tsx
+++ commands/index.tsx
@@ -8,7 +8,7 @@ import {
 	readJsonSync,
 	writeJson,
 } from "fs-extra";
-import { Text, Box, useInput } from "ink";
+import { Text, Box, useInput, useApp } from "ink";
 import useDimensions from "ink-use-stdout-dimensions";
 import TextInput from "ink-text-input";
 import SelectInput from "ink-select-input";
@@ -170,6 +170,7 @@ const Hello = ({ name = "" }) => {
 	const [currentTimeline, setCurrentTimeline] = useState<Tweet[]>([]);
 
 	const [cols, rows] = useDimensions();
+	const { exit } = useApp();
 
 	useEffect(() => {
 		const init = async () => {
