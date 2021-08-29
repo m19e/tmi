51d97d22b48ff68bc27df6fcab4bc14189d24356
m19e <nexus.me7290@gmail.com>
2021-08-30 05:45:31 +0900

    Use divider instead of border

--- src/components/TweetItem.tsx
+++ src/components/TweetItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, Box } from "ink";
-
+import Divider from "ink-divider";
+import useDimensions from "ink-use-stdout-dimensions";
 import { Tweet } from "../types/twitter";
 import { getDisplayTimeAgo } from "../lib";
 
@@ -17,44 +18,54 @@ const TweetItem = ({ tweet, isFocused }: Props) => {
 			: tweet.created_at
 	);
 
+	const [cols] = useDimensions();
+
 	return (
-		<Box
-			flexDirection="column"
-			borderStyle={isFocused ? "singleDouble" : "single"}
-			borderColor={isFocused ? "white" : "gray"}
-		>
-			{tweet.retweeted_status && (
-				<Text dimColor>
-					🔄 {tweet.user.name}
-					{tweet.user.protected && "🔒"} RT
-				</Text>
-			)}
-			<Text>
-				<Text color={tweet.retweeted_status ? "greenBright" : "#00acee"}>
-					{`${t.user.name} @${t.user.screen_name} `}
-					{t.user.protected && "🔒 "}
-				</Text>
-				<Text dimColor>{ago}</Text>
-			</Text>
-			<Text>
-				{t.full_text}
-				{tweet.entities.media && <Text dimColor> (with Media)</Text>}
-			</Text>
-			<Box height={1}>
-				{isFocused && (
-					<>
-						<Box marginRight={2}>
-							<Text>{t.retweet_count ? t.retweet_count + " " : ""}</Text>
-							<Text color={t.retweeted ? "green" : "white"}>RT</Text>
-						</Box>
-						<Box marginRight={2}>
-							<Text>{t.favorite_count ? t.favorite_count + " " : ""}</Text>
-							<Text color={t.favorited ? "yellow" : "white"}>fav</Text>
-						</Box>
-					</>
+		<>
+			<Box
+				flexDirection="column"
+				paddingLeft={isFocused ? 2 : 0}
+				// borderStyle={isFocused ? "singleDouble" : "single"}
+				// borderColor={isFocused ? "white" : "gray"}
+			>
+				{tweet.retweeted_status && (
+					<Text dimColor>
+						🔄 {tweet.user.name}
+						{tweet.user.protected && "🔒"} RT
+					</Text>
 				)}
+				<Text>
+					<Text color={tweet.retweeted_status ? "greenBright" : "#00acee"}>
+						{`${t.user.name} @${t.user.screen_name} `}
+						{t.user.protected && "🔒 "}
+					</Text>
+					<Text dimColor>{ago}</Text>
+				</Text>
+				<Text>
+					{t.full_text}
+					{tweet.entities.media && <Text dimColor> (with Media)</Text>}
+				</Text>
+				<Box height={1}>
+					{isFocused && (
+						<>
+							<Box marginRight={2}>
+								<Text>{t.retweet_count ? t.retweet_count + " " : ""}</Text>
+								<Text color={t.retweeted ? "green" : "white"}>RT</Text>
+							</Box>
+							<Box marginRight={2}>
+								<Text>{t.favorite_count ? t.favorite_count + " " : ""}</Text>
+								<Text color={t.favorited ? "yellow" : "white"}>fav</Text>
+							</Box>
+						</>
+					)}
+				</Box>
 			</Box>
-		</Box>
+			<Divider
+				width={cols}
+				padding={0}
+				dividerColor={isFocused ? "white" : "gray"}
+			/>
+		</>
 	);
 };
 
