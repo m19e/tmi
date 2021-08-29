c354cf709055a15af06805c3a1fae850bdb34a0a
m19e <nexus.me7290@gmail.com>
2021-08-29 16:26:12 +0900

    Remove needless fc

--- commands/index.tsx
+++ commands/index.tsx
@@ -652,61 +652,6 @@ const Timeline = ({
 	);
 };
 
-const TweetBox = ({
-	tweet,
-	isFocused,
-}: {
-	tweet: Tweet;
-	isFocused: boolean;
-}) => {
-	const t = tweet.retweeted_status ?? tweet;
-	const ago = getDisplayTimeAgo(
-		tweet.retweeted_status
-			? tweet.retweeted_status.created_at
-			: tweet.created_at
-	);
-
-	return (
-		<Box
-			flexDirection="column"
-			borderStyle={isFocused ? "singleDouble" : "single"}
-			borderColor={isFocused ? "white" : "gray"}
-		>
-			{tweet.retweeted_status && (
-				<Text dimColor>
-					🔄 {tweet.user.name}
-					{tweet.user.protected && "🔒"} RT
-				</Text>
-			)}
-			<Text>
-				<Text color={tweet.retweeted_status ? "greenBright" : "#00acee"}>
-					{`${t.user.name} @${t.user.screen_name} `}
-					{t.user.protected && "🔒 "}
-				</Text>
-				<Text dimColor>{ago}</Text>
-			</Text>
-			<Text>
-				{t.full_text}
-				{tweet.entities.media && <Text dimColor> (with Media)</Text>}
-			</Text>
-			<Box height={1}>
-				{isFocused && (
-					<>
-						<Box marginRight={2}>
-							<Text>{t.retweet_count ? t.retweet_count + " " : ""}</Text>
-							<Text color={t.retweeted ? "green" : "white"}>RT</Text>
-						</Box>
-						<Box marginRight={2}>
-							<Text>{t.favorite_count ? t.favorite_count + " " : ""}</Text>
-							<Text color={t.favorited ? "yellow" : "white"}>fav</Text>
-						</Box>
-					</>
-				)}
-			</Box>
-		</Box>
-	);
-};
-
 Tink.propTypes = {
 	/// Name of the person to greet
 	name: PropTypes.string,
