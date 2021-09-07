25a2f63dce7f555768826c9f6b767f5673153d8f
m19e <nexus.me7290@gmail.com>
2021-09-07 11:11:49 +0900

    Add get client hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -9,3 +9,5 @@ export const useClient = (): [
 	Twitter | null,
 	(update?: SetStateAction<Twitter | null>) => void
 ] => useAtom(clientAtom);
+
+export const getClient = () => useAtom(clientAtom)[0];
