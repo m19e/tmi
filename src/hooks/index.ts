7dad53d2f6d8b136704dd39ef9e5fc22068563c7
m19e <nexus.me7290@gmail.com>
2021-09-07 16:09:10 +0900

    Remove get client hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -9,5 +9,3 @@ export const useClient = (): [
 	Twitter | null,
 	(update?: SetStateAction<Twitter | null>) => void
 ] => useAtom(clientAtom);
-
-export const getClient = () => useAtom(clientAtom)[0];
