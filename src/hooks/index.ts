976a9a9e2886c8a876a1a40fd1046ee97de0737a
m19e <nexus.me7290@gmail.com>
2021-09-08 19:05:35 +0900

    Add use-mover hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -7,6 +7,7 @@ import {
 	displayTimelineAtom,
 	cursorIndexAtom,
 	focusIndexAtom,
+	displayTweetsCountAtom,
 } from "../store";
 
 export const useUserId = () => useAtom(userIdAtom);
@@ -23,3 +24,66 @@ export const getDisplayTimeline = () => useAtom(displayTimelineAtom)[0];
 export const useCursorIndex = () => useAtom(cursorIndexAtom);
 
 export const useFocusIndex = () => useAtom(focusIndexAtom);
+
+export const useMover = (): {
+	prev: (update: () => void) => void;
+	next: (update: () => void) => void;
+	pageUp: (update: () => void) => void;
+	pageDown: (update: () => void) => void;
+	top: () => void;
+	bottom: () => void;
+} => {
+	const [count] = useAtom(displayTweetsCountAtom);
+	const [{ length }] = useAtom(timelineAtom);
+	const [cursor, setCursor] = useAtom(cursorIndexAtom);
+	const [focus, setFocus] = useAtom(focusIndexAtom);
+
+	const prev = (update: () => void) => {
+		if (focus === 0) {
+			if (cursor === 0) {
+				update();
+			} else {
+				setCursor((c) => c - 1);
+			}
+		} else {
+			setFocus((f) => f - 1);
+		}
+	};
+	const next = (update: () => void) => {
+		if (focus + 1 === count) {
+			if (cursor + count + 1 > length) {
+				update();
+			} else {
+				setCursor((c) => c + 1);
+			}
+		} else {
+			setFocus((f) => f + 1);
+		}
+	};
+	const pageUp = (update: () => void) => {
+		if (cursor + focus < count) {
+			update();
+		} else {
+			setCursor(Math.max(cursor - count, 0));
+		}
+	};
+	const pageDown = (update: () => void) => {
+		if (cursor + count * 2 > length) {
+			update();
+		} else {
+			setCursor(Math.min(cursor + count, length - count - 1));
+		}
+	};
+	const top = () => {
+		if (cursor !== 0) {
+			setCursor(0);
+		}
+	};
+	const bottom = () => {
+		if (cursor < length - count) {
+			setCursor(length - count);
+		}
+	};
+
+	return { prev, next, pageUp, pageDown, top, bottom };
+};
