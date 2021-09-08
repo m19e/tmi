3f0cf9509f008dff152086c14e313d7abe8ed96b
m19e <nexus.me7290@gmail.com>
2021-09-08 16:26:37 +0900

    Add hooks about timeline handle

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,13 @@
 import { useAtom, SetStateAction } from "jotai";
 import type Twitter from "twitter-lite";
-
-import { userIdAtom, clientAtom } from "../store";
+import {
+	userIdAtom,
+	clientAtom,
+	timelineAtom,
+	displayTimelineAtom,
+	cursorIndexAtom,
+	focusIndexAtom,
+} from "../store";
 
 export const useUserId = () => useAtom(userIdAtom);
 
@@ -9,3 +15,11 @@ export const useClient = (): [
 	Twitter | null,
 	(update?: SetStateAction<Twitter | null>) => void
 ] => useAtom(clientAtom);
+
+export const useTimeline = () => useAtom(timelineAtom);
+
+export const getDisplayTimeline = () => useAtom(displayTimelineAtom)[0];
+
+export const useCursorIndex = () => useAtom(cursorIndexAtom);
+
+export const useFocusIndex = () => useAtom(focusIndexAtom);
