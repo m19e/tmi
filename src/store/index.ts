77b2d170e567da607bbb81f249ebe5028629cb56
m19e <nexus.me7290@gmail.com>
2021-09-08 11:41:34 +0900

    Add atoms about timeline

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,22 @@
 import { atom } from "jotai";
 import type Twitter from "twitter-lite";
+import { Tweet } from "../types/twitter";
 
 export const userIdAtom = atom("");
 
 export const clientAtom = atom<Twitter | null>(null);
+
+export const timelineAtom = atom<Tweet[]>([]);
+
+export const cursorIndexAtom = atom(0);
+
+export const focusIndexAtom = atom(0);
+
+export const displayTweetsCountAtom = atom(5);
+
+export const displayTimeline = atom<Tweet[]>((get) => {
+	const cursor = get(cursorIndexAtom);
+	const count = get(displayTweetsCountAtom);
+
+	return get(timelineAtom).slice(cursor, cursor + count);
+});
