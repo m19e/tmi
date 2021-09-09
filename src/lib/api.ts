dd2f9e85eccab01996f90b98387b9a45b060c77d
m19e <nexus.me7290@gmail.com>
2021-09-09 17:05:03 +0900

    Rule

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,12 +8,12 @@ export const postReply = async (
 		status: string;
 		in_reply_to_status_id: string;
 	}
-): Promise<null | any> => {
+): Promise<null | string> => {
 	try {
 		await client.post("statuses/update", params);
 		return null;
-	} catch (error) {
-		return error;
+	} catch (_error) {
+		return "Error: POST statuses/update";
 	}
 };
 
@@ -22,12 +22,12 @@ export const postDeleteTweet = async (
 	params: {
 		id: string;
 	}
-): Promise<null | any> => {
+): Promise<null | string> => {
 	try {
 		await client.post("statuses/destroy", params);
 		return null;
-	} catch (error) {
-		return error;
+	} catch (_error) {
+		return "Error: POST statuses/destroy";
 	}
 };
 
