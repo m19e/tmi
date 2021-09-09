1545a1cfa2d9acfb58d88b0cdf1658fbbe889ca2
m19e <nexus.me7290@gmail.com>
2021-09-09 13:32:40 +0900

    Add get-list-timeline-params type to root

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+interface DefaultTwitterRequestParams {
+	tweet_mode: "extended";
+	include_entities: true;
+}
+
+export interface GetListTimelineParams extends DefaultTwitterRequestParams {
+	list_id: string;
+	count: number;
+	since_id?: string;
+	max_id?: string;
+}
