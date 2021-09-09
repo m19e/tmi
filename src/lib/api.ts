5625fced96c2aee4ae13cd0fa61649ab02fae128
m19e <nexus.me7290@gmail.com>
2021-09-09 17:10:01 +0900

    Fix apis order

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,31 @@ import type Twitter from "twitter-lite";
 import type { GetListTweetsParams } from "../types";
 import type { Tweet } from "../types/twitter";
 
+// GET request
+export const getListTweets = async (
+	client: Twitter,
+	params: GetListTweetsParams
+): Promise<Tweet[] | string> => {
+	try {
+		return await client.get("lists/statuses", params);
+	} catch (_error) {
+		return "Error: GET lists/statuses";
+	}
+};
+
+// POST request
+export const postTweet = async (
+	client: Twitter,
+	params: { status: string }
+): Promise<null | string> => {
+	try {
+		await client.post("statuses/update", params);
+		return null;
+	} catch (_error) {
+		return "Error: POST statuses/update";
+	}
+};
+
 export const postReply = async (
 	client: Twitter,
 	params: {
@@ -28,28 +53,5 @@ export const postDeleteTweet = async (
 		return null;
 	} catch (_error) {
 		return "Error: POST statuses/destroy";
-	}
-};
-
-export const getListTweets = async (
-	client: Twitter,
-	params: GetListTweetsParams
-): Promise<Tweet[] | string> => {
-	try {
-		return await client.get("lists/statuses", params);
-	} catch (_error) {
-		return "Error: GET lists/statuses";
-	}
-};
-
-export const postTweet = async (
-	client: Twitter,
-	params: { status: string }
-): Promise<null | string> => {
-	try {
-		await client.post("statuses/update", params);
-		return null;
-	} catch (_error) {
-		return "Error: POST statuses/update";
 	}
 };
