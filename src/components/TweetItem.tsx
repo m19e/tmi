a059548d25e2d1cf37ec2a0bca1de857f9a46bfe
m19e <nexus.me7290@gmail.com>
2021-09-15 05:18:27 +0900

    Add border-less tweet-item fc

--- src/components/TweetItem.tsx
+++ src/components/TweetItem.tsx
@@ -84,50 +84,81 @@ const Borderless = ({ tweet, isFocused, inFav, inRT }: Props) => {
 	const generatedColor = stc(t.user.screen_name);
 
 	return (
-		<Box flexDirection="column" paddingLeft={isFocused ? 4 : 0}>
-			{tweet.retweeted_status && (
-				<Text dimColor>
-					🔄 {tweet.user.name}
-					{tweet.user.protected && "🔒"} RT
-				</Text>
-			)}
-			<Text>
-				<Text color={generatedColor}>
-					{`${t.user.name} @${t.user.screen_name} `}
-					{t.user.protected && "🔒 "}
-				</Text>
-				<Text dimColor>{ago}</Text>
-			</Text>
-			{t.in_reply_to_screen_name && (
-				<Text>
-					<Text dimColor>Replying to </Text>
-					<Text color="#00acee">@{t.in_reply_to_screen_name}</Text>
-				</Text>
-			)}
-			<Text>
-				{t.full_text}
-				{t.entities.media && <Text dimColor> (with Media)</Text>}
-			</Text>
-			<Quoted tweet={t.quoted_status} />
-			<Box height={1}>
+		<Box>
+			<Box width={2} height={2} flexDirection="column">
 				{isFocused && (
 					<>
-						<Box>
-							<Text>{t.retweet_count ? t.retweet_count + " " : ""}</Text>
-							<Text color={t.retweeted ? "green" : "white"}>RT</Text>
-							<Box marginX={1}>
-								<Loader loading={inRT} namedColor="green" />
-							</Box>
-						</Box>
-						<Box>
-							<Text>{t.favorite_count ? t.favorite_count + " " : ""}</Text>
-							<Text color={t.favorited ? "yellow" : "white"}>fav</Text>
-							<Box marginX={1}>
-								<Loader loading={inFav} namedColor="yellow" />
-							</Box>
-						</Box>
+						<Text color="#00acee">█</Text>
+						<Text color="#00acee">█</Text>
 					</>
 				)}
+			</Box>
+			<Box flexDirection="column" marginBottom={1}>
+				<Text>
+					<Text bold color={generatedColor}>
+						{t.user.name}{" "}
+					</Text>
+					<Text>
+						(@{t.user.screen_name}) {t.user.protected && "🔒 "}
+					</Text>
+					<Text>[{ago}] </Text>
+					{tweet.retweeted_status && (
+						<Text>
+							(<Text bold>@{tweet.user.screen_name}</Text> RT){" "}
+						</Text>
+					)}
+					<Text color="yellow">
+						{inFav ? (
+							<>
+								<Loader loading={inFav} namedColor="yellow" />{" "}
+							</>
+						) : (
+							<>{t.favorited ? "█ " : ""}</>
+						)}
+						{t.favorite_count ? `${t.favorite_count}fav ` : " "}
+					</Text>
+					<Text color="green">
+						{inRT ? (
+							<>
+								<Loader loading={inRT} namedColor="green" />{" "}
+							</>
+						) : (
+							<>{t.retweeted ? "█ " : ""}</>
+						)}
+						{t.retweet_count ? `${t.retweet_count}RT ` : " "}
+					</Text>
+				</Text>
+				{t.in_reply_to_screen_name && (
+					<Text>
+						<Text dimColor>Replying to </Text>
+						<Text color="#00acee">@{t.in_reply_to_screen_name}</Text>
+					</Text>
+				)}
+				<Text>
+					{t.full_text}
+					{t.entities.media && <Text dimColor> (with Media)</Text>}
+				</Text>
+				<Quoted tweet={t.quoted_status} />
+				{/* <Box height={1}>
+					{isFocused && (
+						<>
+							<Box>
+								<Text>{t.retweet_count ? t.retweet_count + " " : ""}</Text>
+								<Text color={t.retweeted ? "green" : "white"}>RT</Text>
+								<Box marginX={1}>
+									<Loader loading={inRT} namedColor="green" />
+								</Box>
+							</Box>
+							<Box>
+								<Text>{t.favorite_count ? t.favorite_count + " " : ""}</Text>
+								<Text color={t.favorited ? "yellow" : "white"}>fav</Text>
+								<Box marginX={1}>
+									<Loader loading={inFav} namedColor="yellow" />
+								</Box>
+							</Box>
+						</>
+					)}
+				</Box> */}
 			</Box>
 		</Box>
 	);
