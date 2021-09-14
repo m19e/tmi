a736e3ec87fc4933cc03e808d04ea9b77d0649da
m19e <nexus.me7290@gmail.com>
2021-09-15 02:09:18 +0900

    Color generated by user screen_name

--- src/components/TweetItem.tsx
+++ src/components/TweetItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, Box } from "ink";
 import Divider from "ink-divider";
 import useDimensions from "ink-use-stdout-dimensions";
+import stc from "string-to-color";
 import { Tweet } from "../types/twitter";
 import { getDisplayTimeAgo } from "../lib";
 import Loader from "./Loader";
@@ -77,4 +78,59 @@ const TweetItem = ({ tweet, isFocused, inFav, inRT }: Props) => {
 	);
 };
 
-export default TweetItem;
+const Borderless = ({ tweet, isFocused, inFav, inRT }: Props) => {
+	const t = tweet.retweeted_status ?? tweet;
+	const ago = getDisplayTimeAgo(t.created_at);
+	const generatedColor = stc(t.user.screen_name);
+
+	return (
+		<Box flexDirection="column" paddingLeft={isFocused ? 4 : 0}>
+			{tweet.retweeted_status && (
+				<Text dimColor>
+					🔄 {tweet.user.name}
+					{tweet.user.protected && "🔒"} RT
+				</Text>
+			)}
+			<Text>
+				<Text color={generatedColor}>
+					{`${t.user.name} @${t.user.screen_name} `}
+					{t.user.protected && "🔒 "}
+				</Text>
+				<Text dimColor>{ago}</Text>
+			</Text>
+			{t.in_reply_to_screen_name && (
+				<Text>
+					<Text dimColor>Replying to </Text>
+					<Text color="#00acee">@{t.in_reply_to_screen_name}</Text>
+				</Text>
+			)}
+			<Text>
+				{t.full_text}
+				{t.entities.media && <Text dimColor> (with Media)</Text>}
+			</Text>
+			<Quoted tweet={t.quoted_status} />
+			<Box height={1}>
+				{isFocused && (
+					<>
+						<Box>
+							<Text>{t.retweet_count ? t.retweet_count + " " : ""}</Text>
+							<Text color={t.retweeted ? "green" : "white"}>RT</Text>
+							<Box marginX={1}>
+								<Loader loading={inRT} namedColor="green" />
+							</Box>
+						</Box>
+						<Box>
+							<Text>{t.favorite_count ? t.favorite_count + " " : ""}</Text>
+							<Text color={t.favorited ? "yellow" : "white"}>fav</Text>
+							<Box marginX={1}>
+								<Loader loading={inFav} namedColor="yellow" />
+							</Box>
+						</Box>
+					</>
+				)}
+			</Box>
+		</Box>
+	);
+};
+
+export default Borderless;
