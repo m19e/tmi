0f367f691ecb5002dd25511f82dd756865e600d5
m19e <nexus.me7290@gmail.com>
2021-09-16 06:37:59 +0900

    Add border-less detail fc

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -12,6 +12,7 @@ import { getDisplayTime } from "../lib";
 import { postTweetApi, postReplyApi, postDeleteTweetApi } from "../lib/api";
 import { useUserId, useClient } from "../hooks";
 import Loader from "./Loader";
+import TweetItem from "./TweetItem";
 import Quoted from "./Quoted";
 
 type TimelineProcess =
@@ -354,6 +355,286 @@ const Detail = ({
 	);
 };
 
+const Borderless = ({
+	tweet,
+	onMention,
+	onRemove,
+	isTweetOpen,
+	setIsTweetOpen,
+	inProcess,
+	setInProcess,
+}: {
+	tweet: Tweet;
+	onMention: () => void;
+	onRemove: (options?: { redraft: boolean }) => void;
+	isTweetOpen: boolean;
+	setIsTweetOpen: (b: boolean) => void;
+	inProcess: TimelineProcess;
+	setInProcess: (p: TimelineProcess) => void;
+}) => {
+	const [client] = useClient();
+	const [userId] = useUserId();
+	const [cols] = useDimensions();
+
+	const [tweetMode, setTweetMode] = useState<"none" | "reply" | "quote">(
+		"none"
+	);
+	const openReplyTweet = () => {
+		setTweetMode("reply");
+		setIsTweetOpen(true);
+	};
+	const openQuoteTweet = () => {
+		setTweetMode("quote");
+		setIsTweetOpen(true);
+	};
+
+	const [waitReturn, setWaitReturn] = useState(false);
+	const [tweetText, setTweetText] = useState("");
+	const [{ weightedLength, valid }, setParsedTweet] = useState<ParsedTweet>(
+		parseTweet("")
+	);
+
+	const t = tweet.retweeted_status ?? tweet;
+	const time = getDisplayTime(t.created_at);
+	const displayFavRT =
+		t.retweet_count !== 0 ||
+		t.favorite_count !== 0 ||
+		inProcess === "rt" ||
+		inProcess === "fav";
+	const quoteUrl = `https://twitter.com/${t.user.screen_name}/status/${t.id_str}`;
+
+	const myTweet = t.user.id_str === userId;
+	let selectItems: SelectItemProps[] = [
+		{
+			label: `Tweet to @${t.user.screen_name}`,
+			value: "mention",
+		},
+	];
+	if (myTweet) {
+		selectItems = selectItems.concat([
+			{ label: "Delete", value: "delete" },
+			{ label: "Re-draft", value: "re-draft" },
+		]);
+	} else {
+		selectItems = selectItems.concat([
+			{ label: `Mute @${t.user.screen_name}`, value: "mute-user" },
+			{ label: "Mute Retweets from User", value: "mute-retweets" },
+			{ label: "Mute Quotes from User", value: "mute-quotes" },
+			{ label: `Block @${t.user.screen_name}`, value: "block" },
+		]);
+	}
+	selectItems = selectItems.concat([
+		{
+			label: `Mute "${t.source.replace(/(<([^>]+)>)/gi, "")}"`,
+			value: "mute-client",
+		},
+	]);
+
+	const resetTweetState = () => {
+		setIsTweetOpen(false);
+		setTweetMode("none");
+		setWaitReturn(false);
+		setTweetText("");
+		setParsedTweet(parseTweet(""));
+	};
+
+	const reply = async () => {
+		setInProcess("reply");
+		const error = await postReplyApi(client, {
+			status: tweetText,
+			in_reply_to_status_id: t.id_str,
+		});
+		setInProcess("none");
+		if (error !== null) {
+			// onError()
+			return;
+		}
+		resetTweetState();
+	};
+
+	const quote = async () => {
+		setInProcess("quote");
+		const error = await postTweetApi(client, {
+			status: `${tweetText} ${quoteUrl}`,
+		});
+		setInProcess("none");
+		if (error !== null) {
+			// onError()
+			return;
+		}
+		resetTweetState();
+	};
+
+	const deleteTweet = async (
+		{
+			redraft,
+		}: {
+			redraft: boolean;
+		} = { redraft: false }
+	) => {
+		setInProcess("delete");
+		const error = await postDeleteTweetApi(client, { id: t.id_str });
+		setInProcess("none");
+		if (error !== null) {
+			// onError
+			return;
+		}
+		onRemove({ redraft });
+		resetTweetState();
+	};
+
+	useInput(
+		(input, key) => {
+			if (input === "r") {
+				openReplyTweet();
+			} else if (input === "q") {
+				openQuoteTweet();
+			}
+		},
+		{ isActive: !isTweetOpen && inProcess === "none" }
+	);
+
+	useInput(
+		(input, key) => {
+			if (key.escape) {
+				if (waitReturn) {
+					setWaitReturn(false);
+				} else {
+					// Avoid warning: state update on an unmounted TextInput
+					// Maybe caused by Node.js (single-threaded)?
+					setTimeout(() => {
+						resetTweetState();
+					});
+				}
+			} else if (waitReturn && key.return) {
+				if (tweetMode === "reply") reply();
+				if (tweetMode === "quote") quote();
+			}
+		},
+		{ isActive: isTweetOpen && inProcess === "none" }
+	);
+
+	const handleTweetChange = (value: string) => {
+		setTweetText(value);
+		setParsedTweet(parseTweet(value));
+	};
+
+	const handleQuoteChange = (value: string) => {
+		setTweetText(value);
+		setParsedTweet(parseTweet(`${value} ${quoteUrl}`));
+	};
+
+	const handleSelectMenu = ({ value }: SelectItemProps) => {
+		if (value === "mention") {
+			onMention();
+		} else if (value === "delete") {
+			deleteTweet();
+		} else if (value === "re-draft") {
+			deleteTweet({ redraft: true });
+		}
+	};
+
+	return (
+		<>
+			<Box flexGrow={1} flexDirection="column">
+				<TweetItem
+					tweet={tweet}
+					isFocused={true}
+					inFav={inProcess === "fav"}
+					inRT={inProcess === "rt"}
+				/>
+				<Box flexDirection="column" marginLeft={2} marginBottom={1}>
+					{!isTweetOpen && (
+						<SelectInput
+							items={selectItems}
+							itemComponent={SelectItem}
+							indicatorComponent={Indicator}
+							onSelect={handleSelectMenu}
+						/>
+					)}
+					{isTweetOpen && (
+						<>
+							{tweetMode === "reply" && (
+								<>
+									<Box
+										justifyContent="space-between"
+										width="50%"
+										marginBottom={1}
+									>
+										<Text color="gray">
+											Replying to{" "}
+											<Text color="#00acee">@{t.user.screen_name} </Text>
+											<Loader
+												loading={inProcess === "reply"}
+												rawColor="#00acee"
+											/>
+										</Text>
+										<Text color="gray">{280 - weightedLength}/280</Text>
+									</Box>
+									<Box minHeight={3}>
+										<Text color="white">
+											<TextInput
+												placeholder="Tweet your reply"
+												value={tweetText}
+												onChange={handleTweetChange}
+												onSubmit={() => setWaitReturn(valid)}
+												focus={!waitReturn}
+											/>
+										</Text>
+									</Box>
+								</>
+							)}
+							{tweetMode === "quote" && (
+								<>
+									<Box
+										justifyContent="space-between"
+										width="50%"
+										marginBottom={1}
+									>
+										<Text color="gray">
+											Quote{" "}
+											<Text color="#00acee">@{tweet.user.screen_name}</Text>'s
+											tweet{" "}
+											<Loader
+												loading={inProcess === "quote"}
+												namedColor="green"
+											/>
+										</Text>
+										<Text color="gray">{280 - weightedLength}/280</Text>
+									</Box>
+									<Box minHeight={3} flexDirection="column">
+										<Text color="white">
+											<TextInput
+												placeholder="Add a comment"
+												value={tweetText}
+												onChange={handleQuoteChange}
+												onSubmit={() => setWaitReturn(valid)}
+												focus={!waitReturn}
+											/>
+										</Text>
+										<Quoted tweet={t} />
+									</Box>
+								</>
+							)}
+						</>
+					)}
+				</Box>
+			</Box>
+			{isTweetOpen ? (
+				<>
+					{waitReturn ? (
+						<Text>[Enter] {tweetMode} [ESC] cancel</Text>
+					) : (
+						<Text>[Enter] done [ESC] close</Text>
+					)}
+				</>
+			) : (
+				<Text>[R] reply [Q] quote [T] retweet [F] favorite [ESC] back</Text>
+			)}
+		</>
+	);
+};
+
 type TweetMenuActionTarget = "user" | "retweets" | "quotes" | "client";
 
 type TweetMenuAction =
@@ -387,4 +668,4 @@ const Indicator: FC<{
 	</Box>
 );
 
-export default Detail;
+export default Borderless;
