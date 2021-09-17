abebd6f302cc8aa300d36cca889fd27c0c38bd2a
m19e <nexus.me7290@gmail.com>
2021-09-17 19:48:46 +0900

    Remove loader props type

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,26 +2,6 @@ import React from "react";
 import { Text, TextProps } from "ink";
 import Spinner from "./Spinner";
 
-type ForegroundColor =
-	| "black"
-	| "red"
-	| "green"
-	| "yellow"
-	| "blue"
-	| "magenta"
-	| "cyan"
-	| "white"
-	| "gray"
-	| "grey"
-	| "blackBright"
-	| "redBright"
-	| "greenBright"
-	| "yellowBright"
-	| "blueBright"
-	| "magentaBright"
-	| "cyanBright"
-	| "whiteBright";
-
 type Props = {
 	loading: boolean;
 	color: TextProps["color"];
