5f7f7408d2b44d2a310436defcbd3dd2e23ed357
m19e <nexus.me7290@gmail.com>
2021-09-17 19:40:09 +0900

    Add char-counter fc

---
+++ src/components/TweetCharCounter.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { FC } from "react";
+import { Text } from "ink";
+import { fallbackSymbols } from "../lib/sindresorhus/figures";
+
+type Props = {
+	invalid: boolean;
+	length: number;
+	limit?: number;
+};
+
+const Counter: FC<Props> = ({ invalid, length, limit = 280 }) => (
+	<Text color={invalid ? "redBright" : "gray"}>
+		{invalid && <>[{fallbackSymbols.warning}] </>}
+		{length}/{limit}
+	</Text>
+);
+
+export default Counter;
