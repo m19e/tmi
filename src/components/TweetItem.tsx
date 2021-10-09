321fde556232769fc32349448f2e6f988ad3218e
m19e <nexus.me7290@gmail.com>
2021-10-10 03:45:59 +0900

    Remove old tweet-item

--- src/components/TweetItem.tsx
+++ src/components/TweetItem.tsx
@@ -16,69 +16,6 @@ type Props = {
 	isFocused?: boolean;
 };
 
-const TweetItem = ({ tweet, isFocused = true, inFav, inRT }: Props) => {
-	const t = tweet.retweeted_status ?? tweet;
-	const ago = getDisplayTimeAgo(t.created_at);
-
-	const [cols] = useDimensions();
-
-	return (
-		<>
-			<Box flexDirection="column" paddingLeft={isFocused ? 4 : 0}>
-				{tweet.retweeted_status && (
-					<Text dimColor>
-						🔄 {tweet.user.name}
-						{tweet.user.protected && "🔒"} RT
-					</Text>
-				)}
-				<Text>
-					<Text color={tweet.retweeted_status ? "greenBright" : "#00acee"}>
-						{`${t.user.name} @${t.user.screen_name} `}
-						{t.user.protected && "🔒 "}
-					</Text>
-					<Text dimColor>{ago}</Text>
-				</Text>
-				{t.in_reply_to_screen_name && (
-					<Text>
-						<Text dimColor>Replying to </Text>
-						<Text color="#00acee">@{t.in_reply_to_screen_name}</Text>
-					</Text>
-				)}
-				<Text>
-					{t.full_text}
-					{t.entities.media && <Text dimColor> (with Media)</Text>}
-				</Text>
-				<Quoted tweet={t.quoted_status} />
-				<Box height={1}>
-					{isFocused && (
-						<>
-							<Box>
-								<Text>{t.retweet_count ? t.retweet_count + " " : ""}</Text>
-								<Text color={t.retweeted ? "green" : "white"}>RT</Text>
-								<Box marginX={1}>
-									<Loader loading={inRT} color="green" />
-								</Box>
-							</Box>
-							<Box>
-								<Text>{t.favorite_count ? t.favorite_count + " " : ""}</Text>
-								<Text color={t.favorited ? "yellow" : "white"}>fav</Text>
-								<Box marginX={1}>
-									<Loader loading={inFav} color="yellow" />
-								</Box>
-							</Box>
-						</>
-					)}
-				</Box>
-			</Box>
-			<Divider
-				width={cols}
-				padding={0}
-				dividerColor={isFocused ? "white" : "gray"}
-			/>
-		</>
-	);
-};
-
 const Borderless = ({ tweet, isFocused = true, inFav, inRT }: Props) => {
 	const t = tweet.retweeted_status ?? tweet;
 	const ago = getDisplayTimeAgo(t.created_at);
