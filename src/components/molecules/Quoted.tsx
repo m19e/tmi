28d81a58479a43fadeb8881cfbf1593f709503f1
m19e <nexus.me7290@gmail.com>
2021-10-11 02:14:26 +0900

    Remove old quoted

--- src/components/molecules/Quoted.tsx
+++ src/components/molecules/Quoted.tsx
@@ -5,27 +5,6 @@ import type { Tweet } from "../../types/twitter";
 import { getDisplayTimeAgo } from "../../lib";
 import figures from "../../lib/sindresorhus/figures";
 
-const Quoted = ({ tweet }: { tweet: Tweet | null }) => {
-	if (!tweet) return null;
-	const ago = getDisplayTimeAgo(tweet.created_at);
-
-	return (
-		<Box flexDirection="column" borderStyle="round" borderColor="gray">
-			<Text>
-				<Text color="greenBright">
-					{`${tweet.user.name} @${tweet.user.screen_name} `}
-					{tweet.user.protected && "🔒 "}
-				</Text>
-				<Text dimColor>{ago}</Text>
-			</Text>
-			<Text>
-				{tweet.full_text}
-				{tweet.entities.media && <Text dimColor> (with Media)</Text>}
-			</Text>
-		</Box>
-	);
-};
-
 const Borderless = ({ tweet }: { tweet: Tweet | null }) => {
 	if (!tweet) return null;
 	const ago = getDisplayTimeAgo(tweet.created_at);
