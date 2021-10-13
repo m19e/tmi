de423616313ad28609bdeb1305779be745cbce12
m19e <nexus.me7290@gmail.com>
2021-10-13 18:39:55 +0900

    Add response error helper

---
+++ src/lib/helpers/index.ts
@@ -0,0 +1,27 @@
+import { ApiResponseError } from "twitter-api-v2";
+import { HandledResponseError } from "../../types";
+
+type Endpoint = "lists/list";
+
+export const handleResponseError = (
+	error: any,
+	method: "GET" | "POST",
+	endpoint: Endpoint
+): HandledResponseError => {
+	if (
+		error instanceof ApiResponseError &&
+		error.rateLimitError &&
+		error.rateLimit
+	) {
+		return {
+			rateLimit: true,
+			message: `${method} ${endpoint}: Rate limit will reset on ${new Date(
+				error.rateLimit.reset
+			)}`,
+		};
+	}
+	return {
+		rateLimit: false,
+		message: `Error: ${method} ${endpoint}`,
+	};
+};
