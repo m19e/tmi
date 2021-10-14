bc50a4f366c0092cd33b9c6ef8404a3c5276a5b6
m19e <nexus.me7290@gmail.com>
2021-10-14 20:30:10 +0900

    Use TweetV1 for root Tweet type

--- src/types/twitter.ts
+++ src/types/twitter.ts
@@ -1,3 +1,5 @@
+import type { TweetV1 } from "twitter-api-v2";
+
 export interface List {
 	id: number;
 	id_str: string;
@@ -20,44 +22,46 @@ export interface TrimmedList {
 	mode: "public" | "private";
 }
 
-export interface Tweet {
-	contributors?: null;
-	coordinates?: Coordinates | null;
-	created_at: string;
-	current_user_retweet?: TrimmedUser;
-	display_text_range?: [number, number] | null;
-	entities: Entities;
-	extended_entities?: ExtendedEntities | null;
-	favorite_count: number;
-	favorited: boolean;
-	full_text: string;
-	id_str: string;
-	id: number;
-	in_reply_to_status?: Tweet | string | null;
-	in_reply_to_screen_name?: string | null;
-	in_reply_to_status_id_str?: string | null;
-	in_reply_to_status_id?: number | null;
-	in_reply_to_user_id_str?: string | null;
-	in_reply_to_user_id?: number | null;
-	is_quote_status: boolean;
-	lang?: string | null;
-	place?: Place | null;
-	possibly_sensitive?: boolean | null;
-	quoted_status_id_str?: string | null;
-	quoted_status_id?: number | null;
-	quoted_status_permalink?: QuotedStatusPermalink | null;
-	quoted_status?: Tweet | null;
-	retweet_count: number;
-	retweeted_status?: Tweet | null;
-	retweeted: boolean;
-	scopes?: Scope | null;
-	source: string;
-	truncated: boolean;
-	user: User;
-	withheld_copyright?: boolean | null;
-	withheld_in_countries?: string[] | null;
-	withheld_scope?: string | null;
-}
+// export interface Tweet {
+// 	contributors?: null;
+// 	coordinates?: Coordinates | null;
+// 	created_at: string;
+// 	current_user_retweet?: TrimmedUser;
+// 	display_text_range?: [number, number] | null;
+// 	entities: Entities;
+// 	extended_entities?: ExtendedEntities | null;
+// 	favorite_count: number;
+// 	favorited: boolean;
+// 	full_text: string;
+// 	id_str: string;
+// 	id: number;
+// 	in_reply_to_status?: Tweet | string | null;
+// 	in_reply_to_screen_name?: string | null;
+// 	in_reply_to_status_id_str?: string | null;
+// 	in_reply_to_status_id?: number | null;
+// 	in_reply_to_user_id_str?: string | null;
+// 	in_reply_to_user_id?: number | null;
+// 	is_quote_status: boolean;
+// 	lang?: string | null;
+// 	place?: Place | null;
+// 	possibly_sensitive?: boolean | null;
+// 	quoted_status_id_str?: string | null;
+// 	quoted_status_id?: number | null;
+// 	quoted_status_permalink?: QuotedStatusPermalink | null;
+// 	quoted_status?: Tweet | null;
+// 	retweet_count: number;
+// 	retweeted_status?: Tweet | null;
+// 	retweeted: boolean;
+// 	scopes?: Scope | null;
+// 	source: string;
+// 	truncated: boolean;
+// 	user: User;
+// 	withheld_copyright?: boolean | null;
+// 	withheld_in_countries?: string[] | null;
+// 	withheld_scope?: string | null;
+// }
+
+export interface Tweet extends TweetV1 {}
 
 export interface User {
 	created_at: string;
