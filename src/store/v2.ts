3c78e0de054ed0f62fe0b72dfd4fee88ae1c5dd4
m19e <nexus.me7290@gmail.com>
2021-10-16 06:26:51 +0900

    Add list paginator atom

--- src/store/v2.ts
+++ src/store/v2.ts
@@ -1,7 +1,9 @@
 import { atom } from "jotai";
-import type { TwitterApi } from "twitter-api-v2";
+import type { TwitterApi, ListTimelineV1Paginator } from "twitter-api-v2";
 import { UserConfig } from "../types";
 
 export const userConfigAtom = atom<UserConfig | null>(null);
 
 export const twitterClientAtom = atom<TwitterApi | null>(null);
+
+export const listPaginatorAtom = atom<ListTimelineV1Paginator | null>(null);
