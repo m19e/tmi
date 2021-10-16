2d0951b99d47733a252e8eabd20f59d0979b0e3f
m19e <nexus.me7290@gmail.com>
2021-10-16 15:23:08 +0900

    Add list-timeline, pagination-cursors atom

--- src/store/v2.ts
+++ src/store/v2.ts
@@ -1,5 +1,9 @@
 import { atom } from "jotai";
-import type { TwitterApi, ListTimelineV1Paginator } from "twitter-api-v2";
+import type {
+	TwitterApi,
+	ListTimelineV1Paginator,
+	TweetV1,
+} from "twitter-api-v2";
 import { UserConfig } from "../types";
 
 export const userConfigAtom = atom<UserConfig | null>(null);
@@ -7,3 +11,18 @@ export const userConfigAtom = atom<UserConfig | null>(null);
 export const twitterClientAtom = atom<TwitterApi | null>(null);
 
 export const listPaginatorAtom = atom<ListTimelineV1Paginator | null>(null);
+
+export const listTimelineAtom = atom<TweetV1[]>([]);
+
+export const listTimelineCursorsAtom = atom<{
+	since_id: string;
+	max_id: string;
+}>((get) => {
+	const tweets = get(listTimelineAtom);
+	if (!tweets.length) {
+		return { since_id: "0", max_id: "0" };
+	}
+	const since_id = (BigInt(tweets[0].id_str) + BigInt(1)).toString();
+	const max_id = (BigInt(tweets.slice(-1)[0].id_str) - BigInt(1)).toString();
+	return { since_id, max_id };
+});
