aa465d922242b6817e2ac9a03790bade107e6c92
m19e <nexus.me7290@gmail.com>
2021-10-19 20:54:05 +0900

    Remove type for twitter-lite client

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,26 +2,7 @@ import { useAtom, SetStateAction } from "jotai";
 import { requestResultAtom, errorAtom, hintAtom } from "../store";
 import { GetListTweetsParams, TimelineHintKey } from "../types";
 import type { List, Tweet } from "../types/twitter";
-import type { HandledErrorResponse } from "../lib/api";
 import { hintMap } from "../consts";
-
-type PostApiRequestWithID = (params: { id: string }) => Promise<null | string>;
-
-interface ClientApi {
-	getTweet: (params: { id: string }) => Promise<Tweet | string>;
-	getUserLists: () => Promise<List[] | HandledErrorResponse>;
-	getListTimeline: (params: GetListTweetsParams) => Promise<Tweet[] | string>;
-	tweet: (params: { status: string }) => Promise<null | string>;
-	reply: (params: {
-		status: string;
-		in_reply_to_status_id: string;
-	}) => Promise<null | string>;
-	deleteTweet: PostApiRequestWithID;
-	favorite: PostApiRequestWithID;
-	unfavorite: PostApiRequestWithID;
-	retweet: PostApiRequestWithID;
-	unretweet: PostApiRequestWithID;
-}
 
 export const useRequestResult = (): [
 	string | undefined,
