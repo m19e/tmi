3626cfcebb2d8f862d55b8404e2d9c455c1fc0d9
m19e <nexus.me7290@gmail.com>
2021-10-19 20:43:07 +0900

    Use UserV1 type

--- src/types/twitter.ts
+++ src/types/twitter.ts
@@ -1,4 +1,4 @@
-import type { TweetV1, ListV1 } from "twitter-api-v2";
+import type { TweetV1, ListV1, UserV1 } from "twitter-api-v2";
 
 export interface List extends ListV1 {}
 
@@ -10,28 +10,4 @@ export interface TrimmedList {
 
 export interface Tweet extends TweetV1 {}
 
-export interface User {
-	created_at: string;
-	default_profile_image: boolean;
-	default_profile: boolean;
-	description?: string | null;
-	entities: UserEntities;
-	favourites_count: number;
-	followers_count: number;
-	friends_count: number;
-	id_str: string;
-	id: number;
-	listed_count: number;
-	location?: string | null;
-	name: string;
-	profile_banner_url?: string;
-	profile_image_url_https: string;
-	protected: boolean;
-	screen_name: string;
-	status?: Tweet;
-	statuses_count: number;
-	url?: string | null;
-	verified: boolean;
-	withheld_in_countries?: string[];
-	withheld_scope?: string;
-}
+export interface User extends UserV1 {}
