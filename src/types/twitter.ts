77875dc11c43d7bdb4f4dc16d2a956b5cd2eb604
m19e <nexus.me7290@gmail.com>
2021-10-19 21:39:32 +0900

    Remove types for old client

--- src/types/twitter.ts
+++ src/types/twitter.ts
@@ -1,13 +1,5 @@
-import type { TweetV1, ListV1, UserV1 } from "twitter-api-v2";
-
-export interface List extends ListV1 {}
-
 export interface TrimmedList {
 	id_str: string;
 	name: string;
 	mode: "public" | "private";
 }
-
-export interface Tweet extends TweetV1 {}
-
-export interface User extends UserV1 {}
