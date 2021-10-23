b46f95a7f4aa19460951bce4a7af577f387cad0d
m19e <nexus.me7290@gmail.com>
2021-10-23 17:27:57 +0900

    [WIP]Add home command

---
+++ commands/home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AuthContainer } from "../src/components/AuthContainer";
+
+/// Home timeline command
+const HomeCommand = () => {
+	// return <AuthContainer />;
+};
+
+export default HomeCommand;
