c65d90a25646bf20fe4e034e23ece522423d52ca
m19e <nexus.me7290@gmail.com>
2021-10-23 22:41:41 +0900

    Set columns

--- commands/index.tsx
+++ commands/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { AuthContainer } from "../src/components/AuthContainer";
+import { Columns } from "../src/components/Columns";
 import { List as ListPage } from "../src/components/pages/List";
 
 /// Index command
 const IndexCommand = () => {
 	return (
 		<AuthContainer>
-			<ListPage />
+			<Columns />
 		</AuthContainer>
 	);
 };
