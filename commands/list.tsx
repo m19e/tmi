e8daa75db7dd1247a6b9d450e6f524f5bd69966a
m19e <nexus.me7290@gmail.com>
2021-10-23 17:29:45 +0900

    Remove import

--- commands/list.tsx
+++ commands/list.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { AuthContainer } from "../src/components/AuthContainer";
 import { List as ListPage } from "../src/components/pages/List";
 
