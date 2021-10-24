1ff8e9281eacc9394ca9ebef2840fa9030ff1f97
m19e <nexus.me7290@gmail.com>
2021-10-24 23:36:45 +0900

    Remove other component

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,29 +1,25 @@
 import React from "react";
 import type { VFC } from "react";
-import { Box, Text } from "ink";
 import { TabsWithInput, Tab } from "../components/InkTab";
 import { useColumnMap, useCurrentColumn } from "../hooks";
 
 export const Columns: VFC = () => {
 	const [columns] = useColumnMap();
-	const [currentColumn, setColumnKey] = useCurrentColumn();
+	const [, setColumnKey] = useCurrentColumn();
 	const handleTabsChange = (key: string) => setColumnKey(key);
 	const canToggleColumn = true;
 
 	return (
-		<Box flexDirection="column">
-			<Text>Current column: {currentColumn.name}</Text>
-			<TabsWithInput
-				onChange={handleTabsChange}
-				isFocused={canToggleColumn}
-				keyMap={{ useTab: false, useNumbers: true }}
-			>
-				{[...columns.entries()].map(([key, column]) => (
-					<Tab key={key} name={key}>
-						{column.name}
-					</Tab>
-				))}
-			</TabsWithInput>
-		</Box>
+		<TabsWithInput
+			onChange={handleTabsChange}
+			isFocused={canToggleColumn}
+			keyMap={{ useTab: false, useNumbers: true }}
+		>
+			{[...columns.entries()].map(([key, column]) => (
+				<Tab key={key} name={key}>
+					{column.name}
+				</Tab>
+			))}
+		</TabsWithInput>
 	);
 };
