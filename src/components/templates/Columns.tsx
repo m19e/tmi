7551782f9e6da9d9374f9d014cb39dc322540da4
m19e <nexus.me7290@gmail.com>
2021-10-25 21:06:21 +0900

    Set screen rows as min-height columns page

--- src/components/templates/Columns.tsx
+++ src/components/templates/Columns.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Box } from "ink";
+import useDimensions from "ink-use-stdout-dimensions";
 import { Columns } from "../organisms/Columns";
 import { ColumnSwitcher } from "../organisms/ColumnSwitcher";
 
 export const ColumnsTemplate = () => {
+	const [, rows] = useDimensions();
+
 	return (
-		<>
+		<Box flexDirection="column" minHeight={rows}>
 			<Columns />
 			<ColumnSwitcher />
-		</>
+		</Box>
 	);
 };
