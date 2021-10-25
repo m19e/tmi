b2738420133e506db8b1fc154db88e1ce114d3ae
m19e <nexus.me7290@gmail.com>
2021-10-25 21:14:59 +0900

    Display home timeline in toggle column (demo)

--- src/components/organisms/Timeline/Home.tsx
+++ src/components/organisms/Timeline/Home.tsx
@@ -1,6 +1,38 @@
-import React from "react";
-import { Text } from "ink";
+import React, { useEffect } from "react";
+import { Box, Text } from "ink";
+import { useError } from "../../../hooks";
+import { useTwitterApi } from "../../../hooks/v2";
+import { useHomeTimeline } from "../../../hooks/home";
 
 export const HomeTimeline = () => {
-	return <Text>column: Home Timeline</Text>;
+	const api = useTwitterApi();
+	const [timeline, setTimeline] = useHomeTimeline();
+
+	useEffect(() => {
+		const init = async () => {
+			const res = await api.getHomeTweets();
+			if (typeof res === "string") {
+				// setError(res)
+			} else {
+				setTimeline(res);
+			}
+		};
+		if (!timeline.length) {
+			init();
+		}
+	}, []);
+
+	if (!timeline.length) {
+		return <Text>Home Timeline is Empty.</Text>;
+	}
+
+	return (
+		<Box flexDirection="column">
+			{timeline.map((tweet) => (
+				<Text>
+					{tweet.user.screen_name}: {tweet.full_text}
+				</Text>
+			))}
+		</Box>
+	);
 };
