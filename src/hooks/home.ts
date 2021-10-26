c403e7311347a1fae4ae5fa9e6aad6ccb0da3e12
m19e <nexus.me7290@gmail.com>
2021-10-27 01:54:19 +0900

    Import type only

--- src/hooks/home.ts
+++ src/hooks/home.ts
@@ -1,15 +1,13 @@
-import { useAtom, SetStateAction } from "jotai";
+import { useAtom } from "jotai";
+import type { SetStateAction } from "jotai";
+import type { TweetV1 } from "twitter-api-v2";
 import {
 	homeTimelineAtom,
 	displayHomeTimelineAtom,
 	homeFocusedTweetAtom,
 } from "../store/home";
-import type { TweetV1 } from "twitter-api-v2";
 
-export const useHomeTimeline = (): [
-	Array<TweetV1>,
-	(update?: SetStateAction<Array<TweetV1>>) => void
-] => useAtom(homeTimelineAtom);
+export const useHomeTimeline = () => useAtom(homeTimelineAtom);
 
 export const getDisplayTimeline = () => useAtom(displayHomeTimelineAtom)[0];
 
