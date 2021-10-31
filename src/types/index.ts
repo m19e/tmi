5fdc8bdda734bfd5bd69d684a753427d80049119
m19e <nexus.me7290@gmail.com>
2021-10-31 23:27:12 +0900

    Remove needless type

--- src/types/index.ts
+++ src/types/index.ts
@@ -27,18 +27,6 @@ export interface HandledResponseError {
 	message: string;
 }
 
-interface DefaultTwitterRequestParams {
-	tweet_mode: "extended";
-	include_entities: true;
-}
-
-export interface GetListTweetsParams extends DefaultTwitterRequestParams {
-	list_id: string;
-	count: number;
-	since_id?: string;
-	max_id?: string;
-}
-
 export interface UserConfig extends TwitterApiTokens {
 	userId: string;
 	lists: Array<TrimmedList>;
