9e4ecff4e8de1fd66fc651c29f105606ee274355
m19e <nexus.me7290@gmail.com>
2021-11-03 05:35:30 +0900

    Add list timeline provider

---
+++ src/components/organisms/Provider/List.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+	useListTimeline,
+	useMover,
+	useListPaginator,
+	useDisplayTweetsCount,
+	getDisplayTimeline,
+	getFocusedTweet,
+} from "../../../hooks/list";
+import { AbstractTimeline } from "../AbstractTimeline";
+
+export const ListProvider = () => {
+	const displayTimeline = getDisplayTimeline();
+	const [, setTimeline] = useListTimeline();
+	const paginator = useListPaginator();
+	const mover = useMover();
+	const [, countActions] = useDisplayTweetsCount();
+	const focusedTweet = getFocusedTweet();
+
+	return (
+		<AbstractTimeline
+			type="list"
+			timeline={displayTimeline}
+			setTimeline={setTimeline}
+			paginator={paginator}
+			mover={mover}
+			countActions={countActions}
+			focusedTweet={focusedTweet}
+		/>
+	);
+};
