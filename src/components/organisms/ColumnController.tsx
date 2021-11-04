604082426eb7bdfb759fa5cfe528470599182fc2
m19e <nexus.me7290@gmail.com>
2021-11-04 12:18:24 +0900

    Select fetched list, go back columns page

--- src/components/organisms/ColumnController.tsx
+++ src/components/organisms/ColumnController.tsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import type { VFC } from "react";
+import { writeJSON } from "fs-extra";
 import { Text } from "ink";
+import type { TrimmedList } from "../../types/twitter";
 import SelectInput, { Item } from "../molecules/SelectInput";
-import { useColumnMap, useCurrentColumn } from "../../hooks";
+import SelectList from "../molecules/SelectList";
+import { useColumnMap, useUserConfig, useCurrentColumn } from "../../hooks";
+import { useApi } from "../../hooks/api";
 
 type Action = "add" | "sort" | "delete";
 type ColumnType = "home" | "mentions" | "list";
 
-export const ColumnController: VFC = () => {
-	const [columnMap] = useColumnMap();
+export const ColumnController: VFC<{ onBack: () => void }> = ({ onBack }) => {
+	const [columnMap, columnMapUpdater] = useColumnMap();
+	const [config] = useUserConfig();
+	const [, { setColumnKey }] = useCurrentColumn();
+	const api = useApi();
 	const [status, setStatus] = useState<"actions" | "add" | "list">("actions");
 	const [addItems, setAddItems] = useState<Array<Item<ColumnType>>>([]);
+	const [lists, setLists] = useState<TrimmedList[]>([]);
 
 	const actionItems: Array<Item<Action>> = [
 		{ key: "add", label: "Add", value: "add" },
@@ -34,6 +42,37 @@ export const ColumnController: VFC = () => {
 		return selectColumns;
 	};
 
+	const getUserLists = async () => {
+		const res = await api.getLists();
+		// onError
+		if (!Array.isArray(res)) {
+			// setError(res.message);
+			if (res.rateLimit && config.lists.length) {
+				setLists(config.lists);
+				setStatus("list");
+			}
+			return;
+		}
+		if (!res.length) {
+			// setError("Empty: GET lists/list");
+			// exit();
+			return;
+		}
+		const trim: TrimmedList[] = res.map(({ id_str, name, user, mode }) => ({
+			id_str,
+			owner: {
+				id_str: user.id_str,
+				screen_name: user.screen_name,
+				name: user.name,
+			},
+			name,
+			mode,
+		}));
+		await writeJSON(config.filePath, { ...config, lists: trim });
+		setLists(trim);
+		setStatus("list");
+	};
+
 	const handleSelectActions = ({ value }: Item<Action>) => {
 		if (value === "add") {
 			setAddItems(() => filterAddColumns());
@@ -41,14 +80,29 @@ export const ColumnController: VFC = () => {
 		}
 	};
 
-	const handleSelectAddColumn = ({ value }: Item<ColumnType>) => {
+	const handleSelectAddColumn = async ({ value }: Item<ColumnType>) => {
 		if (value === "home") {
 		}
 		if (value === "mentions") {
 		}
 		if (value === "list") {
-			setStatus("list");
+			await getUserLists();
 		}
+	};
+
+	const handleSelectList = ({ value }: Item<TrimmedList>) => {
+		const { id_str, name, owner } = value;
+		const key = `@${owner.screen_name}/${name}`;
+
+		columnMapUpdater.set(key, {
+			type: "list",
+			name: key,
+			list_id: id_str,
+			timeline: [],
+			cursor: 0,
+			focus: 0,
+		});
+		onBack();
 	};
 
 	if (status === "actions") {
@@ -64,6 +118,14 @@ export const ColumnController: VFC = () => {
 			<>
 				<Text>Select column type...</Text>
 				<SelectInput items={addItems} onSelect={handleSelectAddColumn} />
+			</>
+		);
+	}
+	if (status === "list") {
+		return (
+			<>
+				<Text>Select list...</Text>
+				<SelectList lists={lists} onSelect={handleSelectList} />
 			</>
 		);
 	}
