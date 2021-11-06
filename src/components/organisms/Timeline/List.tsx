7e8626cba3ed2ad0b922586555bdddbf4d081efa
m19e <nexus.me7290@gmail.com>
2021-11-06 19:22:11 +0900

    Reset error console

--- src/components/organisms/Timeline/List.tsx
+++ src/components/organisms/Timeline/List.tsx
@@ -63,6 +63,8 @@ export const Timeline = ({ onToggleList }: Props) => {
 		}
 		if (typeof err === "string") {
 			setError(err);
+		} else {
+			setError(undefined);
 		}
 		setInProcess("none");
 	};
