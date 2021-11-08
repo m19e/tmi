2b31f620655ec279742bc32be5229d03dee4e229
m19e <nexus.me7290@gmail.com>
2021-11-08 22:22:50 +0900

    Use list effect in provider level

--- src/components/organisms/Provider/List.tsx
+++ src/components/organisms/Provider/List.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Text } from "ink";
+import { useCurrentColumn, useError, useHint } from "../../../hooks";
 import {
 	useTimelineWithCache,
 	useMover,
@@ -11,20 +13,46 @@ import {
 import { AbstractTimeline } from "../AbstractTimeline";
 
 export const ListProvider = () => {
+	const [column] = useCurrentColumn();
+	const [, setError] = useError();
+	const [, setHintKey] = useHint();
+
 	const displayTimeline = getDisplayTimeline();
 	const [, setTimeline] = useTimelineWithCache();
 	const paginator = useListPaginator();
 	const mover = useMover();
 	const [, countActions] = useDisplayTweetsCount();
 	const focusedTweet = getFocusedTweet();
-
 	const [, { loadPosition }] = usePosition();
 
-	const handleLoadList = (tl: typeof displayTimeline) => {
-		setTimeline(tl);
-		loadPosition();
-	};
+	const [status, setStatus] = useState<"init" | "timeline">("init");
 
+	useEffect(() => {
+		if (column.type === "list") {
+			if (column.timeline.length) {
+				setTimeline(column.timeline);
+				loadPosition();
+				setStatus("timeline");
+				setHintKey("timeline");
+			} else {
+				const f = async () => {
+					const error = await paginator.fetch();
+					if (typeof error === "string") {
+						setError(error);
+					}
+					setStatus("timeline");
+					setHintKey("timeline");
+				};
+				f();
+			}
+		}
+
+		return () => setStatus("init");
+	}, [column.name]);
+
+	if (status === "init") {
+		return <Text>Loading...</Text>;
+	}
 	return (
 		<AbstractTimeline
 			type="list"
@@ -34,7 +62,6 @@ export const ListProvider = () => {
 			mover={mover}
 			countActions={countActions}
 			focusedTweet={focusedTweet}
-			onLoadList={handleLoadList}
 		/>
 	);
 };
