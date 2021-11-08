f5905367891b06ce195d85aac448e184cbc9a7f1
m19e <nexus.me7290@gmail.com>
2021-11-08 22:22:06 +0900

    Use effect in home/mentions provider level

--- src/components/organisms/Provider/Home.tsx
+++ src/components/organisms/Provider/Home.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Text } from "ink";
+import { useCurrentColumn, useError, useHint } from "../../../hooks";
 import {
 	useHomeTimeline,
 	useMover,
@@ -10,6 +12,10 @@ import {
 import { AbstractTimeline } from "../AbstractTimeline";
 
 export const HomeProvider = () => {
+	const [column] = useCurrentColumn();
+	const [, setError] = useError();
+	const [, setHintKey] = useHint();
+
 	const displayTimeline = getDisplayTimeline();
 	const [, setTimeline] = useHomeTimeline();
 	const paginator = useHomePaginator();
@@ -17,6 +23,31 @@ export const HomeProvider = () => {
 	const [, countActions] = useDisplayTweetsCount();
 	const focusedTweet = getFocusedTweet();
 
+	const [status, setStatus] = useState<"init" | "timeline">("init");
+
+	useEffect(() => {
+		if (column.type === "home") {
+			if (focusedTweet) {
+				setStatus("timeline");
+				setHintKey("timeline");
+			} else {
+				const f = async () => {
+					const error = await paginator.fetch();
+					if (typeof error === "string") {
+						setError(error);
+					}
+					setStatus("timeline");
+					setHintKey("timeline");
+				};
+				f();
+			}
+		}
+		return () => setStatus("init");
+	}, [column.type]);
+
+	if (status === "init") {
+		return <Text>Loading...</Text>;
+	}
 	return (
 		<AbstractTimeline
 			type="home"
--- src/components/organisms/Provider/Mentions.tsx
+++ src/components/organisms/Provider/Mentions.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Text } from "ink";
+import { useCurrentColumn, useError, useHint } from "../../../hooks";
 import {
 	useTimeline,
 	useDisplayTweetsCount,
@@ -10,6 +12,10 @@ import {
 import { AbstractTimeline } from "../AbstractTimeline";
 
 export const MentionsProvider = () => {
+	const [column] = useCurrentColumn();
+	const [, setError] = useError();
+	const [, setHintKey] = useHint();
+
 	const displayTimeline = getDisplayTimeline();
 	const [, setTimeline] = useTimeline();
 	const paginator = useMentionsPaginator();
@@ -17,6 +23,31 @@ export const MentionsProvider = () => {
 	const [, countActions] = useDisplayTweetsCount();
 	const focusedTweet = getFocusedTweet();
 
+	const [status, setStatus] = useState<"init" | "timeline">("init");
+
+	useEffect(() => {
+		if (column.type === "mentions") {
+			if (focusedTweet) {
+				setStatus("timeline");
+				setHintKey("timeline");
+			} else {
+				const f = async () => {
+					const error = await paginator.fetch();
+					if (typeof error === "string") {
+						setError(error);
+					}
+					setStatus("timeline");
+					setHintKey("timeline");
+				};
+				f();
+			}
+		}
+		return () => setStatus("init");
+	}, [column.type]);
+
+	if (status === "init") {
+		return <Text>Loading...</Text>;
+	}
 	return (
 		<AbstractTimeline
 			type="home"
