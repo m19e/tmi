1433411c3eb51b41525835535490d560ecb9d312
m19e <nexus.me7290@gmail.com>
2021-11-10 14:06:24 +0900

    [WIP]Add search command

---
+++ commands/search.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AuthContainer } from "../src/components/pages/AuthContainer";
+
+/// Search command
+const SearchCommand = () => {
+	return <AuthContainer>{/* <SearchPage /> */}</AuthContainer>;
+};
+
+export default SearchCommand;
