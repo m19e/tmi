1f502f4eb6c29d603fbdb31de37c200fbabf9b28
m19e <nexus.me7290@gmail.com>
2021-11-10 20:56:58 +0900

    Add search command

--- commands/search.tsx
+++ commands/search.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { AuthContainer } from "../src/components/pages/AuthContainer";
+import { SearchPage } from "../src/components/organisms/Search";
+
+interface Props {
+	query: string;
+}
 
 /// Search command
-const SearchCommand = () => {
-	return <AuthContainer>{/* <SearchPage /> */}</AuthContainer>;
+const SearchCommand = ({ query }: Props) => {
+	return (
+		<AuthContainer>
+			<SearchPage query={query} />
+		</AuthContainer>
+	);
+};
+
+SearchCommand.propTypes = {
+	query: PropTypes.string.isRequired,
 };
 
 export default SearchCommand;
