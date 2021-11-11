ca3b96f69c8a0dd302868b0387551c734e84fb6b
m19e <nexus.me7290@gmail.com>
2021-11-11 16:30:42 +0900

    Wrap command line args query by quote

--- src/components/organisms/Search.tsx
+++ src/components/organisms/Search.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box, Text } from "ink";
+import { Box, Text, useInput } from "ink";
 import TextInput from "ink-text-input";
 import useDimensions from "ink-use-stdout-dimensions";
 import { useError, useHint } from "../../hooks";
@@ -21,7 +21,7 @@ interface Props {
 export const SearchPage = ({ query }: Props) => {
 	const [, rows] = useDimensions();
 	const [, setError] = useError();
-	const [, setHintKey] = useHint();
+	const [{ key: hintKey }, setHintKey] = useHint();
 
 	const displayTimeline = getDisplayTimeline();
 	const [{ length: total }, setTimeline] = useTimeline();
@@ -32,15 +32,18 @@ export const SearchPage = ({ query }: Props) => {
 	const [{ cursor }] = usePosition();
 
 	const [status, setStatus] = useState<"init" | "timeline">("init");
+	const [searchQuery, setSearchQuery] = useState(`"${query}"`);
+	const [isFormOpen, setIsFormOpen] = useState(false);
+	const [formQuery, setFormQuery] = useState(`"${query}"`);
 
 	useEffect(() => {
-		initialFetch();
-
+		initialFetch(`"${query}"`);
 		return () => setStatus("init");
 	}, []);
 
-	const initialFetch = async () => {
-		const error = await paginator.fetch({ q: `"${query}" -RT` });
+	const initialFetch = async (queryText: string) => {
+		const error = await paginator.fetch({ q: `${queryText} -RT` });
+		// const error = await paginator.fetch({ q: query });
 		if (typeof error === "string") {
 			setError(error);
 		}
@@ -48,16 +51,81 @@ export const SearchPage = ({ query }: Props) => {
 		setHintKey("unique/timeline");
 	};
 
-	const Header = () => (
-		<Box justifyContent="space-between" marginBottom={1}>
-			<Text color="#00acee" bold>
-				Search: {query}
-			</Text>
-			<Text>
-				[{cursor + 1}-{cursor + count}/{total}]
-			</Text>
-		</Box>
+	const handleFormSubmit = async (value: string) => {
+		setStatus("init");
+		await initialFetch(value);
+		setSearchQuery(value);
+		setIsFormOpen(false);
+	};
+
+	useInput(
+		(input, key) => {
+			if (input === "s" && !isFormOpen) {
+				setFormQuery(searchQuery);
+				setIsFormOpen(true);
+			} else if (key.escape && isFormOpen) {
+				setTimeout(() => {
+					setIsFormOpen(false);
+				});
+			}
+		},
+		{ isActive: hintKey === "unique/timeline" }
 	);
+
+	const Header = () => {
+		if (isFormOpen) {
+			return (
+				<Box justifyContent="flex-start" marginBottom={1}>
+					<Text color="#00acee" bold>
+						<>Search: </>
+					</Text>
+					<TextInput
+						value={formQuery}
+						onChange={setFormQuery}
+						onSubmit={handleFormSubmit}
+					/>
+				</Box>
+			);
+		}
+		return (
+			<Box justifyContent="space-between" marginBottom={1}>
+				<Text color="#00acee" bold>
+					Search: {searchQuery}
+				</Text>
+				{!!total ? (
+					<Text>
+						[{cursor + 1}-{cursor + count}/{total}]
+					</Text>
+				) : (
+					<Text>[empty]</Text>
+				)}
+			</Box>
+		);
+	};
+
+	const Result = () => {
+		if (!total) {
+			return (
+				<Text bold color="gray">
+					No results for {`"${searchQuery}"`}
+				</Text>
+			);
+		}
+		if (!isFormOpen) {
+			return (
+				<AbstractTimeline
+					type="home"
+					timeline={displayTimeline}
+					setTimeline={setTimeline}
+					paginator={paginator}
+					mover={mover}
+					countActions={countActions}
+					focusedTweet={focusedTweet}
+				/>
+			);
+		}
+		return null;
+	};
 
 	if (status === "init") {
 		return <Text>Loading...</Text>;
@@ -65,15 +133,7 @@ export const SearchPage = ({ query }: Props) => {
 	return (
 		<Box flexDirection="column" minHeight={rows}>
 			<Header />
-			<AbstractTimeline
-				type="home"
-				timeline={displayTimeline}
-				setTimeline={setTimeline}
-				paginator={paginator}
-				mover={mover}
-				countActions={countActions}
-				focusedTweet={focusedTweet}
-			/>
+			<Result />
 		</Box>
 	);
 };
