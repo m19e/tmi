b20a852ef237e2fb322adad69c31c8947de75158
m19e <nexus.me7290@gmail.com>
2021-11-13 18:25:19 +0900

    [WIP]Add search column provider

---
+++ src/components/organisms/Provider/Search.tsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react";
+import { Box, Text, useInput } from "ink";
+import TextInput from "ink-text-input";
+import { useCurrentColumn, useError, useHint } from "../../../hooks";
+import {
+	useTimelineWithCache,
+	useMover,
+	useSearchPaginator,
+	useDisplayTweetsCount,
+	getDisplayTimeline,
+	getFocusedTweet,
+	usePosition,
+} from "../../../hooks/search";
+import { AbstractTimeline } from "../AbstractTimeline";
+
+interface Props {
+	query: string;
+}
+
+export const SearchProvider = ({ query }: Props) => {
+	const [column] = useCurrentColumn();
+	const [, setError] = useError();
+	const [{ key: hintKey }, setHintKey] = useHint();
+
+	const displayTimeline = getDisplayTimeline();
+	const [{ length: total }, setTimeline] = useTimelineWithCache();
+	const paginator = useSearchPaginator();
+	const mover = useMover();
+	const [count, countActions] = useDisplayTweetsCount();
+	const focusedTweet = getFocusedTweet();
+	const [{ cursor }, { loadPosition }] = usePosition();
+
+	const [status, setStatus] = useState<"init" | "timeline">("init");
+	const [searchQuery, setSearchQuery] = useState(`"${query}"`);
+	const [isFormOpen, setIsFormOpen] = useState(false);
+	const [formQuery, setFormQuery] = useState(`"${query}"`);
+
+	useEffect(() => {
+		if (column.type === "search") {
+			if (column.timeline.length) {
+				setTimeline(column.timeline);
+				loadPosition();
+			}
+			setSearchQuery(column.query);
+			setFormQuery(column.query);
+			setStatus("timeline");
+			setHintKey("search/timeline");
+		}
+
+		return () => setStatus("init");
+	}, [column.name]);
+
+	const search = async (q: string) => {
+		const error = await paginator.fetch({ q });
+		if (typeof error === "string") {
+			setError(error);
+		}
+		setStatus("timeline");
+		setHintKey("search/timeline");
+	};
+
+	const handleFormSubmit = async (value: string) => {
+		setStatus("init");
+		await search(value);
+		setSearchQuery(value);
+		setIsFormOpen(false);
+	};
+
+	useInput(
+		(input, key) => {
+			if (input === "s" && !isFormOpen) {
+				setFormQuery(searchQuery);
+				setIsFormOpen(true);
+				setHintKey("search/timeline/form");
+			} else if (key.escape && isFormOpen) {
+				setTimeout(() => {
+					setIsFormOpen(false);
+					setHintKey("search/timeline");
+				});
+			}
+		},
+		{
+			isActive:
+				hintKey === "search/timeline" || hintKey === "search/timeline/form",
+		}
+	);
+
+	const Header = () => {
+		if (isFormOpen) {
+			return (
+				<Box justifyContent="flex-start" marginBottom={1}>
+					<Text color="#00acee" bold>
+						<>Search: </>
+					</Text>
+					<TextInput
+						value={formQuery}
+						onChange={setFormQuery}
+						onSubmit={handleFormSubmit}
+					/>
+				</Box>
+			);
+		}
+		return (
+			<Box justifyContent="space-between" marginBottom={1}>
+				<Text color="#00acee" bold>
+					Search: {searchQuery}
+				</Text>
+				{!!total ? (
+					<Text>
+						[{cursor + 1}-{cursor + count}/{total}]
+					</Text>
+				) : (
+					<Text>[empty]</Text>
+				)}
+			</Box>
+		);
+	};
+
+	if (status === "init") {
+		return <Text>Loading...</Text>;
+	}
+	return (
+		<Box flexDirection="column" flexGrow={1}>
+			<Header />
+			{(() => {
+				if (!total) {
+					return (
+						<Text bold color="gray">
+							No results for {`"${searchQuery}"`}
+						</Text>
+					);
+				}
+				if (!isFormOpen) {
+					return (
+						<AbstractTimeline
+							type="search"
+							timeline={displayTimeline}
+							setTimeline={setTimeline}
+							paginator={paginator}
+							mover={mover}
+							countActions={countActions}
+							focusedTweet={focusedTweet}
+						/>
+					);
+				}
+				return null;
+			})()}
+		</Box>
+	);
+};
