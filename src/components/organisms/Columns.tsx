aee2dad70abac996af83d465fcd2781d0cfa2609
m19e <nexus.me7290@gmail.com>
2021-11-14 19:33:06 +0900

    Display search query in column header
    activate toggle column when search/timeline hint-key
    cache search timeline position before toggle column

--- src/components/organisms/Columns.tsx
+++ src/components/organisms/Columns.tsx
@@ -1,26 +1,47 @@
 import React, { useMemo } from "react";
 import type { VFC } from "react";
-import { Box } from "ink";
+import { Box, Text } from "ink";
 import { TabsWithInput, Tab } from "../InkTab";
 import { useColumnMap, useCurrentColumn, useHint } from "../../hooks";
-import { usePosition } from "../../hooks/list";
+import { usePosition as useListPosition } from "../../hooks/list";
+import { usePosition as useSearchPosition } from "../../hooks/search";
 
 export const Columns: VFC = () => {
 	const [columns] = useColumnMap();
 	const [currentColumn, { setColumnKey }] = useCurrentColumn();
 	const [{ key: hintKey }] = useHint();
-	const [, { cachePosition }] = usePosition();
-	const canToggleColumn = useMemo(() => hintKey === "timeline", [hintKey]);
+	const [, { cachePosition: cacheListPosition }] = useListPosition();
+	const [, { cachePosition: cacheSearchPosition }] = useSearchPosition();
+	const canToggleColumn = useMemo(
+		() => hintKey === "timeline" || hintKey === "search/timeline",
+		[hintKey]
+	);
 
 	const handleTabsChange = (key: string) => {
 		if (currentColumn.type === "list") {
-			cachePosition();
+			cacheListPosition();
+		} else if (currentColumn.type === "search") {
+			cacheSearchPosition();
 		}
 		setColumnKey(key);
 	};
 
 	return (
-		<Box paddingBottom={1}>
+		<Box flexDirection="column" paddingBottom={1}>
+			{/* <Text>
+				{JSON.stringify(
+					[...columns.values()].map((c) => {
+						if (c.type === "list" || c.type === "search") {
+							const { timeline, cursor, focus, ...filtered } = c;
+							return {
+								...filtered,
+								tl: timeline.slice(0, 2).map((t) => t.user.screen_name),
+							};
+						}
+						return c;
+					})
+				)}
+			</Text> */}
 			<TabsWithInput
 				onChange={handleTabsChange}
 				isFocused={canToggleColumn}
@@ -28,7 +49,11 @@ export const Columns: VFC = () => {
 			>
 				{[...columns.entries()].map(([key, column]) => (
 					<Tab key={key} name={key}>
-						{column.name}
+						{column.type === "search" ? (
+							<>{`"${column.query}"`}</>
+						) : (
+							<>{column.name}</>
+						)}
 					</Tab>
 				))}
 			</TabsWithInput>
