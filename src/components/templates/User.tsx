94605ab626115baec6e4640094aa471e59aeb66c
m19e <nexus.me7290@gmail.com>
2021-12-07 01:15:59 +0900

    Shift up funcs from user-menu

--- src/components/templates/User.tsx
+++ src/components/templates/User.tsx
@@ -250,6 +250,115 @@ export const UserSub = ({ sname }: Props) => {
 		f();
 	}, []);
 
+	const makeCheckedMenu = (u: UserV1, rel: FriendshipV1["relationship"]) => {
+		const myself = rel.source.id_str === rel.target.id_str;
+
+		let actions: Item<UserMenuAction>[] = [
+			{
+				label: `${u.statuses_count} tweets`,
+				value: "tweets",
+			},
+			{
+				label: `${u.friends_count} follows`,
+				value: "following",
+			},
+			{
+				label: `${u.followers_count} followers`,
+				value: "followed",
+			},
+			{
+				label: `${u.favourites_count} favorites`,
+				value: "favorites",
+			},
+			{
+				label: `${u.listed_count} listed`,
+				value: "listed",
+			},
+			{
+				label: "Add to / Remove from lists",
+				value: "list/add-remove",
+			},
+		];
+		if (myself) {
+			actions = [...actions, { label: "Edit profile", value: "profile" }];
+		} else {
+			const follow: Item<UserMenuAction> = rel.source.following
+				? {
+						label: "Unfollow this user",
+						value: "unfollow",
+				  }
+				: {
+						label: "Follow this user",
+						value: "follow",
+				  };
+			actions = [
+				...actions,
+				follow,
+				{ label: "Mute this user", value: "mute" },
+				{ label: "Block this user", value: "block" },
+			];
+		}
+
+		const keyed = actions.map((a) => ({ ...a, key: a.value }));
+		return keyed;
+	};
+
+	const transitionListed = async () => {
+		const { id_str } = user;
+		const res = await api.getUserListed({
+			user_id: id_str,
+			count: 1000,
+		});
+		if (typeof res === "string") {
+			return;
+		}
+		const { lists, ...cursors } = res.data;
+		setDebugConsole(
+			JSON.stringify(
+				[
+					{ length: lists.length, ...cursors },
+					lists.map((l) => `@${l.user.screen_name}/${l.name}`),
+				],
+				null,
+				2
+			)
+		);
+	};
+
+	const handleSelectMenu = ({ value: action }: Item<UserMenuAction>) => {
+		if (action === "tweets") {
+			// "statuses/user_timeline"
+			// implemented
+			// api.userTimeline(userId)
+		} else if (action === "following") {
+			// yet
+		} else if (action === "followed") {
+			// yet
+		} else if (action === "favorites") {
+			// yet
+		} else if (action === "listed") {
+			transitionListed();
+		} else if (action === "list/add-remove") {
+			// "lists/members/create" or "lists/members/create_all"
+			// "lists/members/destroy" or "lists/members/destroy_all"
+			// implemented
+			// api.addListMembers({ list_id, user_id })
+			// api.addListMembers({ list_id, user_id[] })
+			// api.removeListMembers({ list_id, user_id })
+			// api.removeListMembers({ list_id, user_id[] })
+		} else if (action === "profile") {
+			// implemented
+		} else if (action === "follow") {
+			// yet
+		} else if (action === "unfollow") {
+			// yet
+		} else if (action === "mute") {
+			// yet
+		} else if (action === "block") {
+			// yet
+		}
+	};
+
 	if (status === "load") {
 		return <Text>Loading...</Text>;
 	}
