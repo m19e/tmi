87d622388d24554a25d0f41cc3f910ca85b95872
m19e <nexus.me7290@gmail.com>
2022-02-17 19:52:10 +0900

    Remove use previous hook

--- src/components/templates/User.tsx
+++ src/components/templates/User.tsx
@@ -121,14 +121,6 @@ const usePositiveCounter = (initialValue?: number): ReturnType => {
 	};
 };
 
-function usePrevious<T>(value: T): T | undefined {
-	const ref = useRef<T>();
-	useEffect(() => {
-		ref.current = value;
-	});
-	return ref.current;
-}
-
 interface Props {
 	sname: string;
 }
