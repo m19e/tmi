70db5339b354a7733793e44cdae2c110b0f40738
m19e <nexus.me7290@gmail.com>
2022-03-11 18:45:36 +0900

    Remove import

--- src/components/templates/User.tsx
+++ src/components/templates/User.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from "react";
-import type { VFC } from "react";
 import { Box, Text, useInput } from "ink";
 import useDimensions from "ink-use-stdout-dimensions";
 import type {
