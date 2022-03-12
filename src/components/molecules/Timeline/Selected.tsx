509d85e89670514832e833beb283108fc29b0340
m19e <nexus.me7290@gmail.com>
2022-03-12 20:06:36 +0900

    Add tweet-menu organ (WIP)

--- src/components/molecules/Timeline/Selected.tsx
+++ src/components/molecules/Timeline/Selected.tsx
@@ -3,12 +3,16 @@ import { Box } from "ink";
 import type { TweetItemProps } from "./types";
 import { TweetItem } from "./TweetItem";
 import { TweetIndicator } from "./TweetIndicator";
+import { TweetMenu } from "../../organisms/TweetMenu";
 
 export const Selected: VFC<TweetItemProps> = ({ value: tweet }) => {
 	return (
-		<Box>
-			<TweetIndicator isSelected={true} />
-			<TweetItem tweet={tweet} />
+		<Box flexDirection="column" flexGrow={1}>
+			<Box>
+				<TweetIndicator isSelected={true} />
+				<TweetItem tweet={tweet} />
+			</Box>
+			<TweetMenu />
 		</Box>
 	);
 };
---
+++ src/components/organisms/TweetMenu.tsx
@@ -0,0 +1,3 @@
+export const TweetMenu = () => {
+	return null;
+};
