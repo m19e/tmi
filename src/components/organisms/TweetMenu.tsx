af5a254a81b6e590b8309c80d80945d3c2fa889e
m19e <nexus.me7290@gmail.com>
2022-03-13 13:51:42 +0900

    Add tweet-menu organ (WIP)

--- src/components/organisms/TweetMenu.tsx
+++ src/components/organisms/TweetMenu.tsx
@@ -1,3 +1,17 @@
-export const TweetMenu = () => {
+import type { VFC } from "react";
+import type { TweetV1 } from "twitter-api-v2";
+import { useUserConfig } from "../../hooks";
+
+interface Props {
+	tweet: TweetV1;
+}
+
+export const TweetMenu: VFC<Props> = ({ tweet }) => {
+	const [{ userId }] = useUserConfig();
+
+	const t = tweet.retweeted_status ?? tweet;
+	const myTweet = t.user.id_str === userId;
+	// const quoteUrl = `https://twitter.com/${t.user.screen_name}/status/${t.id_str}`;
+
 	return null;
 };
