720aa7fe87bb91d3a48156578aecdce5015596d2
m19e <nexus.me7290@gmail.com>
2022-03-19 22:02:35 +0900

    Remove props for selected-component

--- src/components/molecules/Timeline/index.tsx
+++ src/components/molecules/Timeline/index.tsx
@@ -5,7 +5,6 @@ import type { Item } from "../SelectInput";
 import type { TweetItemProps, Updater } from "./types";
 import { TweetItem } from "./TweetItem";
 import { TweetIndicator } from "./TweetIndicator";
-import { curriedSelected } from "./Selected";
 
 const TweetItemWrapper: VFC<TweetItemProps> = ({ value }) => {
 	return <TweetItem tweet={value} />;
@@ -15,25 +14,20 @@ interface Props {
 	tweets: TweetV1[];
 	onSelectTweet: (item: { value: TweetV1 }) => void;
 	onHighlightTweet: (item: { value: TweetV1 }) => void;
-	updater: Updater;
 	limit: number;
-	forceUnselect: boolean;
 }
 
 export const Timeline = ({
 	tweets,
 	onSelectTweet,
 	onHighlightTweet,
-	updater,
 	limit,
-	forceUnselect,
 }: Props) => {
 	const items: Item<TweetV1>[] = tweets.map((t) => ({
 		key: t.id_str,
 		label: t.full_text,
 		value: t,
 	}));
-	const Selected = curriedSelected(updater);
 
 	return (
 		<NoRotateSelect
@@ -42,9 +36,7 @@ export const Timeline = ({
 			onHighlight={onHighlightTweet}
 			indicatorComponent={TweetIndicator}
 			itemComponent={TweetItemWrapper}
-			selectedComponent={Selected}
 			limit={limit}
-			forceUnselect={forceUnselect}
 		/>
 	);
 };
