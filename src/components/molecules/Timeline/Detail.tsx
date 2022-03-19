2dc8b8612567cca8dabaa90aeb4c924f5f92ca64
m19e <nexus.me7290@gmail.com>
2022-03-19 22:50:15 +0900

    Add timeline-detail

---
+++ src/components/molecules/Timeline/Detail.tsx
@@ -0,0 +1,26 @@
+import type { VFC } from "react";
+import { Box } from "ink";
+import type { TweetV1 } from "twitter-api-v2";
+import type { TweetItemProps, Updater } from "./types";
+import { TweetMenu } from "../../organisms/TweetMenu";
+import { TweetItem } from "./TweetItem";
+import { TweetIndicator } from "./TweetIndicator";
+
+interface Props {
+	tweet: TweetV1;
+	updater: Updater;
+}
+
+export const TweetDetail: VFC<Props> = ({ tweet, updater }) => {
+	return (
+		<Box flexDirection="column">
+			<Box>
+				<TweetIndicator isSelected={true} />
+				<TweetItem tweet={tweet} />
+			</Box>
+			<Box marginLeft={2}>
+				<TweetMenu tweet={tweet} updater={updater} />
+			</Box>
+		</Box>
+	);
+};
